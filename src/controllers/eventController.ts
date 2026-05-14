bbaee19c1e9d1c6373914440b1f014078e8dcad3
ferdisaaa <sptraferdi413@gmail.com>
2026-05-14 09:56:47 +0700

    update Events

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -11,9 +11,9 @@ export const getEvents = (req : Request, res : Response) => {
 
 //2. menyimpan data event
 export const createEvent = (req : Request, res : Response) => {
-    const { name, date, location, description } = req.body;
-    if (!name || !date || !location ) {
-        res.status(500).json({ error: "Nama event harus diisi" });
+    const { id, name, date, location, description } = req.body;
+    if ( !id ||!name || !date || !location || !description) {
+        res.status(500).json({ error: "Semua field harus diisi" });
     };
     const newEvent: Event = {
         id: Date.now(),
@@ -27,8 +27,48 @@ export const createEvent = (req : Request, res : Response) => {
 };
 
 //3. menampilkan data event berdasarkan id
-export const getEventById = (req : Request, res : Response) => {};
+export const getEventById = (req: Request, res: Response) => {
+    const eventId = Number(req.params.id as string);
+    const event = events.find((e) => e.id === eventId);
+    if (event) {
+        res.status(200).json(event);
+    } else {
+        res.status(404).json({ message: "Data event tidak ditemukan" });
+    }
+};
 //4. mengubah event berdasarkan id
-export const updateEvent = (req : Request, res : Response) => {};
+export const updateEvent = (req: Request<{ id: string }>, res: Response) => {
+    const eventId = Number(req.params.id);
+    const { name, date, location, description } = req.body;
+    if (!name || !date || !location || !description) {
+        res.status(400).json({ error: "Semua field harus diisi" });
+    }
+    const index = events.findIndex((e) => e.id === eventId);
+    if (index !== -1 && events[index]) {
+        events[index].name = name;
+        events[index].date = date;
+        events[index].location = location;
+        events[index].description = description;
+        res.status(200).json({ 
+            message: "Data event berhasil diubah", 
+            event: events[index] 
+        });
+    } else {
+        res.status(404).json({ message: "Data event tidak ditemukan" });
+    }
+};
 //5. menghapus event berdasarkan id
-export const deleteEvent = (req : Request, res : Response) => {};
+export const deleteEvent = (req: Request, res: Response) => {
+    const eventId = Number(req.params.id);
+    const index = events.findIndex((e) => e.id === eventId);
+    if (index !== -1) {
+        events.splice(index, 1);
+        res.status(200).json({
+            message: "Data event berhasil dihapus"
+        });
+    } else {
+        res.status(404).json({ 
+            message: "Data event tidak ditemukan" 
+        });
+    }
+};
--- src/routes/eventRoute.ts
+++ src/routes/eventRoute.ts
@@ -1,9 +1,12 @@
 import  express  from "express";
-import { createEvent, getEvents } from "../controllers/eventController";
+import { createEvent, deleteEvent, getEventById, getEvents, updateEvent } from "../controllers/eventController";
 
 
 const router = express.Router();
 
 router.get("/", getEvents);
 router.post("/", createEvent);
+router.get("/:id", getEventById);
+router.put("/:id", updateEvent);
+router.delete("/:id", deleteEvent);
 export default router;
